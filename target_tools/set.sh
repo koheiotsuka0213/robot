c296fee19bcb70904ffc57203d85563da1d5d4a0
Kohei Otsuka <koheiotsuka701@gmail.com>
2017-10-24 22:36:27 +0200

    Updated tartget setup script.

--- target_tools/set.sh
+++ target_tools/set.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Setting enviroment for Rasbery pi....."
+mkdir -p /home/kohei/robot/addLibs/BOOST
 export LC_ALL="C"
 cd /home/kohei/robot/ros_catkin_ws/devel_isolated/
 source setup.bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kohei/robot/addedLibs/BOOST
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/kohei/robot/addLibs/BOOST
 echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 
 echo "Finished setting up the environment...."
